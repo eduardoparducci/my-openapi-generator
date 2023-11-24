f5e00fde31cb16154ee713bcedce1dbb5ce33af6
Eduardo Parducci <eduardo.parducci@staples.com>
2023-11-24 16:07:54 -0300

    initial commit

---
+++ src/main/java/com/example/myopenapigenerator/MyOpenapiGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.myopenapigenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOpenapiGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOpenapiGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myopenapigenerator/MyOpenapiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myopenapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOpenapiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
